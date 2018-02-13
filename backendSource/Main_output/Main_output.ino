c81eb0774ccc080fcdcfdf00a5a86febf87a9699
Fábián Bence <fabianbence99@gmail.com>
2018-02-13 16:59:47 +0100

    Output mqtt msg error, typo hibák, feldolgozó funkciók javítása
    mqtt callback konverziós hiábk javítása, value_change és bool_toggle konvertál char* ot int-be

--- backendSource/Main_output/Main_output.ino
+++ backendSource/Main_output/Main_output.ino
@@ -19,7 +19,7 @@
 //======  Nappali változók  ======//
 bool Livingroom_Lamp_1;
 bool Livingroom_Lamp_2;
-bool Livinroom_Cooler;
+bool Livingroom_Cooler;
 bool Livingroom_Window;
 int Livingroom_Heater;
 int Livingroom_Shades;
@@ -29,7 +29,7 @@ int Livingroom_Mood_B;
 
 
 //======  Bejárat változók  ======//
-bool Frontyard_DoorLock;
+bool Frontyard_Doorlock;
 bool Frontyard_Sprinkler;
 
 //======  Konfiguráció  ======//
@@ -45,7 +45,6 @@ PubSubClient client(wifiClient);
 long lastMsg = 0;
 char* msg;
 int value = 0;
-//void GetIntervals(){}
 
 //======  Wifi  ======//
 void setup_wifi() {
@@ -67,6 +66,28 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+//======  Kimenetek feldolgozása  ======//
+void Bool_Toggle(int PIN_Number,char* message){
+    int value = message[0]-'0';
+    if(value==0){
+        digitalWrite(PIN_Number, LOW);
+      }
+      else if(value==1){
+        digitalWrite(PIN_Number, HIGH);
+      }
+}
+      
+void Value_Change(int PIN_Number, char* message){
+    int value = message[0]-'0';
+    if(value<255 && value>0){
+        digitalWrite(PIN_Number, value);
+    }
+}
+
+void ErrorStream(char* ErrorDescription){
+    client.publish("Config_Error",ErrorDescription);
+}
+
 //======  MQTT Callback  ======//
 void callback(char* topic, byte* payload, unsigned int length) {
   /* DEBUG MODE
@@ -83,55 +104,56 @@ void callback(char* topic, byte* payload, unsigned int length) {
   msg = (char*)payload;
 
   if(strTopic == "Livingroom_Lamp_1"){
-      Bool_Toggle(Livingroom_Lamp_1_PIN, payload);
-      Livingroom_Lamp_1 = payload;
+      Bool_Toggle(Livingroom_Lamp_1_PIN, msg);
+      Livingroom_Lamp_1 = msg;
   }
   else if(strTopic == "Livingroom_Lamp_2"){
-      Bool_Toggle(Livingroom_Lamp_2_PIN, payload);
-      Livingroom_Lamp_2 = payload;
+      Bool_Toggle(Livingroom_Lamp_2_PIN, msg);
+      Livingroom_Lamp_2 = msg;
   }
   else if(strTopic == "Livingroom_Cooler"){
-      Bool_Toggle(Livingroom_Cooler_PIN, payload);
-      Livingroom_Cooler = payload;
+      Bool_Toggle(Livingroom_Cooler_PIN, msg);
+      Livingroom_Cooler = msg;
   }
   else if(strTopic == "Livingroom_Heater"){
-      Value_Change(Livingroom_Heater_PIN, payload);
-      Livingroom_Heater = payload;
+      Value_Change(Livingroom_Heater_PIN, msg);
+      Livingroom_Heater = msg[0]-'0';
   }
   else if(strTopic == "Livingroom_Window"){
-      Bool_Toggle(Livingroom_Window_PIN, payload);
-      Livingroom_Window = payload;
+      Bool_Toggle(Livingroom_Window_PIN, msg);
+      Livingroom_Window = msg;
   }
   else if(strTopic == "Livingroom_Shades"){
-      Value_Change(Livingroom_Shades_PIN, payload);
-      Livingroom_Shades = payload;
+      Value_Change(Livingroom_Shades_PIN, msg);
+      Livingroom_Shades = msg[0]-'0';
   }
   else if(strTopic == "Livingroom_Mood_R"){
-      Value_Change(Livingroom_Mood_R_PIN, payload);
-      Livingroom_Mood_R = payload;
-      
+      Value_Change(Livingroom_Mood_R_PIN, msg);
+      Livingroom_Mood_R = msg[0]-'0';
+  }
   else if(strTopic == "Livingroom_Mood_G"){
-      Value_Change(Livingroom_Mood_G_PIN, payload);
-      Livingroom_Mood_G = payload;
+      Value_Change(Livingroom_Mood_G_PIN, msg);
+      Livingroom_Mood_G = msg[0]-'0';
   }
   else if(strTopic == "Livingroom_Mood_B"){
-      Value_Change(Livingroom_Mood_B_PIN, payload);
-      Livingroom_Mood_B = payload;
+      Value_Change(Livingroom_Mood_B_PIN, msg);
+      Livingroom_Mood_B = msg[0]-'0';
   }
-  else if(strTopic == "Frontayrd_Doorlock"){
-      Bool_Toggle(Frontayrd_Doorlock_PIN, payload);
-      Frontayrd_Doorlock = payload;
+  else if(strTopic == "Frontyard_Doorlock"){
+      Bool_Toggle(Frontyard_Doorlock_PIN, msg);
+      Frontyard_Doorlock = msg;
   }
-  else if(strTopic == "Frontayrd_Sprinkler"){
-      Bool_Toggle(Frontayrd_Sprinkler_PIN, payload);
-      Frontayrd_Sprinkler = payload;
+  else if(strTopic == "Frontyard_Sprinkler"){
+      Bool_Toggle(Frontyard_Sprinkler_PIN, msg);
+      Frontyard_Sprinkler = msg;
   }
   else{
     Serial.println("Unknown topic: ");
     Serial.print(strTopic);
     Serial.println("Value: ");
-    Serial.println((char*)payload); 
+    Serial.println(msg); 
   }
+}
 
 void reconnect() {
   while (!client.connected()) {
@@ -150,35 +172,14 @@ void reconnect() {
   }
 }
 
-
-//======  Kimenetek feldolgozása  ======//
-void Bool_Toggle(int PIN_Number,int value){
-    if(value==0){
-        digitalWrite(PIN_Number, LOW);
-      }
-      else if(value==1){
-        digitalWrite(PIN_Number, HIGH);
-      }
-}
-      
-void Value_Change(int PIN_Number, int value){
-    if(value<255 && value>0){
-        digitalWrite(PIN_Number, value);
-    }
-}
-
-void ErrorStream(string ErrorDescription){
-    client.publish("Config_Error",ErrorDescription);
-}
-
 //======  Fő futás  ======//
 void setup() {
   setup_wifi();
-  //client.setServer(mqtt_server,mqttPort);
-  //client.setCallback(callback);
+  client.setServer(mqtt_server,mqttPort);
+  client.setCallback(callback);
 }
 
 void loop() {
-  //if (!client.connected()) {reconnect();}
-  //client.loop();
+  if (!client.connected()) {reconnect();}
+  client.loop();
 }

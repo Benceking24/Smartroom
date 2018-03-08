fac3b73a02db2faa4f87698bd38b5917fc1a94b8
Recan19 <36275649+Recan19@users.noreply.github.com>
2018-03-08 17:47:18 +0100

    values send

--- pages/java.js
+++ pages/java.js
@@ -9,12 +9,39 @@ $( document ).ready(function() {
 	hangulatWrapper.find("input").on('change', function() {
 		var inputvalue=$(this).val();
 		$("#hangulat").attr("src",inputvalue + ".png");
+		
+		if(inputvalue=="piros"){
+			SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Mood;" + "#ff0000");
+			console.log(inputvalue);
+		}
+		if(inputvalue=="zold"){
+			SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Mood;" + "#00ff00");
+			console.log(inputvalue);
+		}
+		if(inputvalue=="kek"){
+			SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Mood;" + "#0000ff");
+			console.log(inputvalue);
+		}
+		if(inputvalue=="nincs"){
+			SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Mood;" + "#000000");
+			console.log(inputvalue);
+		}
 	});;
 	
 	var redonyWrapper=$("#redony");
 	redonyWrapper.find("input").on('change', function() {
 		var inputvalue=$(this).val();
 		$("#redbelul").height(inputvalue);
+		
+		if(inputvalue=="0")
+		{ SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Shades;" + 0);
+		}if(inputvalue=="75")
+		{ SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Shades;" + 50);
+		}else if(inputvalue=="145"){
+			SmartRoomSocket.send("Neumann/SmartRoom/Livingroom/Shades;" + 100);
+		}
+
+			
 	});;
 
 	$(".switch").on('change', function() {
@@ -24,7 +51,7 @@ $( document ).ready(function() {
 			allas=1;
 		}
 		console.log(inputvalue,allas);
-		 martRoomSocket.send(inputvalue +allas); 
+		 SmartRoomSocket.send(inputvalue +allas); 
 	});;
 	
 	//192.168.10.110
